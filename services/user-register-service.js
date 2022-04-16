c8b6d9952996564080c96ff3a784ab96be718029
Gilberto Carlos de Melo <gilbertotads@gmail.com>
2022-04-16 11:15:38 -0300

    correction to request work api/vi/user - POST

--- services/user-register-service.js
+++ services/user-register-service.js
@@ -1,9 +1,22 @@
 export const registerUser = (body) => {
+    const submitButton = document.querySelector('.form_submit');
+    
+    const errorHandler = () => {
+        submitButton.classList.remove('success');
+        submitButton.classList.add('error');
+        submitButton.textContent = "Error :(";
+    }
+
+    const successHandler = () => {
+        submitButton.classList.remove('error');
+        submitButton.classList.add('success');
+        submitButton.textContent = "Sent! :)";
+    }
+
     fetch('http://localhost:8080/api/v1/user', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            'No-Cors':""
         },
         body: JSON.stringify(body)
     }).then((response) => {
